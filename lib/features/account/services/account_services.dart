f2bfc6840ee31834ab7e96544f3778c919fc0991
hero-BA <suad.hero@gmail.com>
2022-12-03 19:46:19 +0100

    Added Log Out Functionality

--- lib/features/account/services/account_services.dart
+++ lib/features/account/services/account_services.dart
@@ -4,10 +4,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_amazon_clone/constants/error_handling.dart';
 import 'package:flutter_amazon_clone/constants/global_variables.dart';
 import 'package:flutter_amazon_clone/constants/utils.dart';
+import 'package:flutter_amazon_clone/features/auth/screens/auth_screen.dart';
 import 'package:flutter_amazon_clone/models/order.dart';
 import 'package:flutter_amazon_clone/providers/user_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AccountServices {
   Future<List<Order>> fetchMyOrders({required BuildContext context}) async {
@@ -38,5 +40,22 @@ class AccountServices {
       showSnackBar(context, err.toString());
     }
     return orderList;
+  }
+
+// Log out functionality
+  void logOut(BuildContext context) async {
+    try {
+      SharedPreferences sharedPreferences =
+          await SharedPreferences.getInstance();
+      await sharedPreferences.setString('x-auth-token', '');
+
+      Navigator.pushNamedAndRemoveUntil(
+        context,
+        AuthScreen.routeName,
+        (route) => false,
+      );
+    } catch (err) {
+      showSnackBar(context, err.toString());
+    }
   }
 }
--- lib/features/account/widgets/top_buttons.dart
+++ lib/features/account/widgets/top_buttons.dart
@@ -1,14 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_amazon_clone/features/account/services/account_services.dart';
 import 'package:flutter_amazon_clone/features/account/widgets/account_button.dart';
 
-class TopButtons extends StatefulWidget {
+class TopButtons extends StatelessWidget {
   const TopButtons({super.key});
 
-  @override
-  State<TopButtons> createState() => _TopButtonsState();
-}
-
-class _TopButtonsState extends State<TopButtons> {
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -24,7 +20,10 @@ class _TopButtonsState extends State<TopButtons> {
         ),
         Row(
           children: [
-            AccountButton(text: 'Log Out', onTap: (() {})),
+            AccountButton(
+              text: 'Log Out',
+              onTap: () => AccountServices().logOut(context),
+            ),
             AccountButton(text: 'Your Wish List', onTap: (() {}))
           ],
         )
